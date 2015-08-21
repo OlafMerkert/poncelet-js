b9c8e9f331a8f3ac61676e5c15e935fbdebea046
Olaf Merkert <olaf@m-merkert.de>
2015-08-21 17:55:37 +0200

    move selection function to interface module

--- poncelet-interface.js
+++ poncelet-interface.js
@@ -32,6 +32,48 @@ function resetPoncelet() {
     pc.reset();
 }
 
+function PointSelector(numberOfPoints, action) {
+    this.numberOfPoints = numberOfPoints;
+    this.points = [];
+    this.action = action;
+    this.addPoint = function (event) {
+        if (this.points.length < this.numberOfPoints) {
+            var point = new Point(event.pageX - this.offsetX,
+                                  event.pageY - this.offsetY);
+            this.points.push(point);
+            this.pc.draw(point);
+            if (this.points.length >= this.numberOfPoints) {
+                this.action(this.points);
+            }
+        } else {
+            // clean up events
+            $(this.pc.canvas).off("click");
+        }
+    };
+    this.askNextPoint = function (pc) {
+        this.pc = pc;
+        if (this.points.length < this.numberOfPoints) {
+            var $canvas = $(pc.canvas);
+            this.offsetX = $canvas.offset().left;
+            this.offsetY = $canvas.offset().top;
+            var ps = this;
+            $canvas.click(function (event) {
+                ps.addPoint(event);
+            });
+        }
+    };
+}
+
+function CircleSelector(pc, action) {
+    var ps = new PointSelector(
+        3,
+        function (points) {
+            var circle = circleFromPoints(points[0], points[1], points[2]);
+            pc.draw(circle);
+            action(circle);
+        });
+    ps.askNextPoint(pc);
+}
 
 function selectRedCircle() {
     // var pc = getPonceletCanvas();
