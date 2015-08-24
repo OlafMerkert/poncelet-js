e918ab44d4b992c41fb59839f20c932f429a8694
Olaf Merkert <olaf@m-merkert.de>
2015-08-24 20:33:45 +0200

    setup the lisp web server for the Poncelet game page

---
+++ server.lisp
@@ -0,0 +1,33 @@
+(defpackage :poncelet-server-config
+  (:use :cl :ol :web-utils)
+  (:export))
+
+(in-package :poncelet-server-config)
+
+;; serve the html and scripts of the site
+(defpar base-url "/~olaf/math/poncelet/")
+(defpar base-path #P"~/Projekte/poncelet-js/")
+
+(defun file-url (f)
+  (mkstr base-url f))
+
+(defun file-path (f)
+  (merge-pathnames f base-path))
+
+(setup-static-content (file-url "") (file-path "poncelet.html"))
+
+(dolist (script '("plane-geometry.js"
+                  "raphael-helper.js"
+                  "poncelet-interface.js"))
+  (setup-static-content (file-url script) (file-path script)))
+
+;; get web libraries
+(dolist (lib '(:jquery
+               :bootstrap
+               :numeric
+               :raphaeljs))
+  (load-web-library lib))
+
+
+;; finally, tell the server about the nice app
+(register-web-application "The Game of Poncelet" base-url)
